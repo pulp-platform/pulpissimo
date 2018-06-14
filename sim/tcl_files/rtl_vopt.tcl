4dc51aebf5303ae2d470ab2779552bc317855822
bluew <bluewww@users.noreply.github.com>
2018-06-14 17:32:11 +0200

    Enable debugdb for ips

--- sim/tcl_files/rtl_vopt.tcl
+++ sim/tcl_files/rtl_vopt.tcl
@@ -25,7 +25,7 @@ set VSIM_RTL_LIBS [regsub -all -- "-L " $VSIM_RTL_LIBS $sub_str]
 
 if {[info exists ::env(VSIM_PATH)]} {
     #eval exec >@stdout vopt +acc=mnpr -o vopt_tb tb_pulp -floatparameters+tb_pulp -Ldir $::env(VSIM_PATH)/modelsim_libs $VSIM_IP_LIBS $VSIM_RTL_LIBS -work work  
-    eval exec >@stdout vopt +acc=mnpr -o vopt_tb tb_pulp -floatparameters+tb_pulp  $VSIM_IP_LIBS $VSIM_RTL_LIBS -work work 
+    eval exec >@stdout vopt -debugdb -fsmdebug +acc=mnpr -o vopt_tb tb_pulp -floatparameters+tb_pulp  $VSIM_IP_LIBS $VSIM_RTL_LIBS -work work 
 } else {
     eval exec >@stdout vopt +acc=mnpr -o vopt_pulp_chip pulp_chip $VSIM_IP_LIBS $VSIM_RTL_LIBS -work pulpissimo_lib
 }
