3a31d62c727cc3e4c1ea804558adae5344e4422b
bluew <bluewww@users.noreply.github.com>
2019-02-19 15:12:00 +0100

    Add also dm_mem, dm_csrs and dm_sba to dm wave file

--- sim/waves/dm.tcl
+++ sim/waves/dm.tcl
@@ -1,8 +1,20 @@
 # add dm
 set dm [find instances -recursive -bydu dm_top -nodu]
+set dm_mem  [find instances -recursive -bydu dm_mem -nodu]
+set dm_csrs [find instances -recursive -bydu dm_csrs -nodu]
+set dm_sba  [find instances -recursive -bydu dm_sba -nodu]
 
 if {$dm ne ""} {
   add wave -group "DM"                                     $dm/*
+}
+if {$dm_mem ne ""} {
+  add wave -group "DM" -group "dm_mem"                     $dm_mem/*
+}
+if {$dm_csrs ne ""} {
+  add wave -group "DM" -group "dm_csrs"                    $dm_csrs/*
+}
+if {$dm_sba ne ""} {
+  add wave -group "DM" -group "dm_sba"                     $dm_sba/*
 }
 
 configure wave -namecolwidth  250
