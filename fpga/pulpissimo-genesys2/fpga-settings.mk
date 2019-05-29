6654d4ffc6182a57da15df05a4946b72e63229ca
Manuel Eggimann <meggimann@iis.ee.ethz.ch>
2019-05-29 16:01:19 +0200

    Change size of L2 memory to match the size assumed in pulp-sdk

--- fpga/pulpissimo-genesys2/fpga-settings.mk
+++ fpga/pulpissimo-genesys2/fpga-settings.mk
@@ -5,7 +5,7 @@ export FC_CLK_PERIOD_NS=100
 export PER_CLK_PERIOD_NS=200
 export SLOW_CLK_PERIOD_NS=200
 #Must also change the localparam 'L2_BANK_SIZE' in pulp_soc.sv accordingly
-export INTERLEAVED_BANK_SIZE=16384
+export INTERLEAVED_BANK_SIZE=28672
 #Must also change the localparam 'L2_BANK_SIZE_PRI' in pulp_soc.sv accordingly
 export PRIVATE_BANK_SIZE=8192
 $(info Setting environment variables for $(BOARD) board)
