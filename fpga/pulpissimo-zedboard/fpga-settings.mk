9eb419da4f2e699bed0385a2649e4d01e2e5c246
Manuel Eggimann <meggimann@iis.ee.ethz.ch>
2020-11-23 11:36:39 +0100

    Decrease default clock frequency on zedboard to 16MHz to meet timing with FPU

--- fpga/pulpissimo-zedboard/fpga-settings.mk
+++ fpga/pulpissimo-zedboard/fpga-settings.mk
@@ -1,7 +1,7 @@
 export BOARD=zedboard
 export XILINX_PART=xc7z020clg484-1
 export XILINX_BOARD=em.avnet.com:zed:part0:1.4
-export FC_CLK_PERIOD_NS=50
+export FC_CLK_PERIOD_NS=62.5
 export PER_CLK_PERIOD_NS=100
 export SLOW_CLK_PERIOD_NS=30517
 #Must also change the localparam 'L2_BANK_SIZE' in pulp_soc.sv accordingly
