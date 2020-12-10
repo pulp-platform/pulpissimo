7b02a8b5d8215dba69be3025a1ddc67264f8dcb2
Manuel Eggimann <meggimann@iis.ee.ethz.ch>
2020-12-10 12:06:53 +0100

    Parametrize new cluster CDC isolation param in pulp_soc

--- rtl/pulpissimo/soc_domain.sv
+++ rtl/pulpissimo/soc_domain.sv
@@ -264,7 +264,8 @@ module soc_domain #(
       .NBIT_PADMUX                      ( 2                  ),
       .N_UART                           ( N_UART             ),
       .N_SPI                            ( N_SPI              ),
-      .N_I2C                            ( N_I2C              )
+      .N_I2C                            ( N_I2C              ),
+      .ISOLATE_CLUSTER_CDC              ( 1                  )
     ) pulp_soc_i (
         // Outputs
         .boot_l2_i                   (1'b0),
