213e3d9d167734c5d3ed6d0810672e7a831b3190
timsaxe <62411665+timsaxe@users.noreply.github.com>
2021-01-08 21:54:30 -0800

    Update xilinx_pulpissimo.v
    Do not use fpu

--- fpga/pulpissimo-nexys/rtl/xilinx_pulpissimo.v
+++ fpga/pulpissimo-nexys/rtl/xilinx_pulpissimo.v
@@ -78,7 +78,7 @@ module xilinx_pulpissimo
  );
 
   localparam CORE_TYPE = 0; // 0 for RISCY, 1 for IBEX RV32IMC (formerly ZERORISCY), 2 for IBEX RV32EC (formerly MICRORISCY)
-  localparam USE_FPU   = 1;
+  localparam USE_FPU   = 0;
   localparam USE_HWPE  = 0;
 
   wire        ref_clk;
