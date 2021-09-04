7ee4b6a2e2e93498ce0273fd372106a0607c1f3f
Peer Adelt <peer.adelt@hni.uni-paderborn.de>
2021-09-04 17:28:14 +0200

    pulpissimo-vcu108: fpga-shell for Xilinx VCU108 Board

---
+++ fpga/pulpissimo-vcu108/fpga-settings.mk
@@ -0,0 +1,7 @@
+export BOARD=vcu108
+export XILINX_PART=xcvu095-ffva2104-2-e
+export XILINX_BOARD=xilinx.com:vcu108:part0:1.7
+export FC_CLK_PERIOD_NS=50
+export PER_CLK_PERIOD_NS=100
+export SLOW_CLK_PERIOD_NS=30517
+$(info Setting environment variables for $(BOARD) board)
